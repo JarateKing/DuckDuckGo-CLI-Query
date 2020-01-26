af15995bdf24de93641215d0198690dfbe3cd5ad
JarateKing <scmacintosh@upei.ca>
2020-01-26 16:59:02 -0400

    improve query results

--- query.py
+++ query.py
@@ -2,8 +2,8 @@ import requests
 import sys
 import json
 
-args = '+'.join(sys.argv[1:])
-response = requests.get("https://api.duckduckgo.com/?q=" + args + "&format=json")
+query = 'what+is+' + ('+'.join(sys.argv[1:]))
+response = requests.get("https://api.duckduckgo.com/?q=" + query + "&format=json")
 
 if (response.status_code == 200):
 	values = response.json()
